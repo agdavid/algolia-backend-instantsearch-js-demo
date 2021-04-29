a2ec3c8d9fa709445dfb37ead642d13820603c1d
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-04-29 10:03:19 -0400

    create custom search client

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
 console.log("Client side JS file loaded");
 
-const searchClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+const algoliaClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+// create custom client
+const searchClient = {
+    ...algoliaClient,
+    search(requests) {
+        return algoliaClient.search(requests);
+    },
+};
+
 const search = instantsearch({
     indexName: 'instant_search',
     searchClient,
