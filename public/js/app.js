e7a3482f7a18bd2af53e611f96445770086ff084
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-04-28 20:33:17 -0400

    submit search query

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
 console.log("Client side JS file loaded");
+
+const searchForm = document.querySelector('form');
+const searchBar = document.querySelector('input');
+
+searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const query = searchBar.value;
+    fetchResults(query);
+});
+
+const fetchResults = query => {
+    console.log(`The query is: ${query}`);
+}
