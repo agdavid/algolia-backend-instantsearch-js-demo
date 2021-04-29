727220607e75123790d0c9e18c7047f760733d6d
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-04-28 20:14:59 -0400

    working express server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const path = require('path');
 const express = require('express');
-const env = require('dotenv').config({ path: '../.env'});
+const dotenvExtended = require('dotenv-extended');
+const dotenvParseVariables = require('dotenv-parse-variables');
+
+// load .env variables
+dotenvParseVariables(
+    dotenvExtended.load({
+      // assign variables to process.env object for accessibility
+      assignToProcessEnv: true,
+      // displays "missing .env file", no need in production, where we use real env variables
+      silent: process.env.APP_ENV === 'production',
+      errorOnMissing: true,
+      // also use process.env to fill variables in, only in production
+      includeProcessEnv: process.env.APP_ENV === 'production',
+    })
+  );
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +24,11 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+// initial view
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(port, ()=> {
     console.log(`Server is up on Port ${port}`);
